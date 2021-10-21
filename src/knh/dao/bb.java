f0beb03b2b0f9c0a9b0316f2bd6a72131599ad03
dus404 <dus404@naver.com>
2021-10-21 17:58:53 +0900

    ss

--- src/knh/dao/bb.java
+++ src/knh/dao/bb.java
@@ -3,3 +3,10 @@ package knh.dao;
 public class bb {
 
 }
+echo "# jeju-travel" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/jeju-travel/jeju-travel.git
+git push -u origin main
